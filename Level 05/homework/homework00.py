c89d092e04cba8588e8bcf786c57943be64f4d6c
TajMahal568 <giorgadzesaba48@gmail.com>
2024-05-31 14:38:55 +0400

    commit

---
+++ Level 05/homework/homework00.py
@@ -0,0 +1,48 @@
+# task 1: Discuss what happened in this code, which best practices we used, write comments and explain why these best practices were needed
+
+dog_name1 = "Rick"
+dog_name2 = "John"
+dog_name3 = "Max"
+
+
+print("First dog's name is" + " " + dog_name1 + ". " + "Second dog's name is" + " " + dog_name2 + ".")
+
+
+# In this code I used two best practices: Snake_Case, Case Sensitivity
+
+# These best practices were necessary for the computer to read this code as soon as possible and understandably
+
+# And in this code, we first declared the variables and then concatenated those names to form a sentence
+
+
+
+# task 2: Make an example of 5 f strings, use big sentences, and include numbers as well.
+
+
+girl_name = "Lizzie"
+boy_name = "Elijah"
+
+girl_age = 20
+boy_age = 22
+
+girl_weight = 54
+boy_weight = 75
+
+girlfav_color = "red"
+boyfav_color = "black"
+
+girl_hoby = "reading books"
+boy_hoby = "playing football"
+
+sentence1 = f"Girl's name is {girl_name}. Boy's name is {boy_name}."
+
+sentence2 = f"Girl is {girl_age} years old. Boy is {boy_age} years old." 
+
+sentence3 = f"Girl's weight is {girl_weight}. Boy's weight is {boy_weight}."
+
+sentence4 = f"Girl's favourite color is {girlfav_color}. Boy's favourite color is {boyfav_color}"
+
+sentence5 = f"The girl's hoby is {girl_hoby}. The boy's hoby is {boy_hoby}."
+
+print(sentence5)
+
