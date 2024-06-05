7de31197b97c77e5384622229cd22de5c5cda857
TajMahal568 <giorgadzesaba48@gmail.com>
2024-06-05 18:58:07 +0400

    commit

--- Level 04/homework/homework00.py
+++ Level 04/homework/homework00.py
@@ -20,7 +20,8 @@ pendown()
 
 width(5)
 
-
+color("grey")
+begin_fill()
 forward(700)
 left(90)
 
@@ -40,11 +41,14 @@ forward(200)
 right(90)
 
 
-forward(200)
-right(90)
 
 forward(200)
 right(90)
+
+
+forward(200)
+right(90)
+end_fill()
 
 
 forward(103)
@@ -54,11 +58,14 @@ forward(201)
 
 
 
+
 penup()
 goto(400,-350)
      
 pendown()
 
+color("grey")
+begin_fill()
 right(90)
 forward(200)
 
@@ -70,14 +77,16 @@ forward(200)
 
 left(90)
 forward(100)
+end_fill()
+
 
 penup()
 goto(601,150)
 pendown()
 
 
-
-
+color("red")
+begin_fill()
 left(90)
 forward(10)
 
@@ -92,7 +101,7 @@ forward(200)
 
 left(120)
 forward(15)
-
+end_fill()
 
 
 penup()
@@ -100,7 +109,8 @@ goto(-300,-350)
 pendown()
 
 
-
+color("grey")
+begin_fill()
 right(90)
 right(85)
 forward(200)
@@ -115,6 +125,7 @@ right(90)
 forward(100)
 
 right(90)
+end_fill()
 
 penup()
 goto(-500,150)
@@ -123,7 +134,8 @@ pendown()
 
 
 
-
+color("red")
+begin_fill()
 forward(10)
 right(125)
 forward(200)
@@ -145,7 +157,8 @@ goto(-80,50)
     
 pendown()
 
-
+color("grey")
+begin_fill()
 left(85)
 forward(200)
 
@@ -155,6 +168,7 @@ forward(250)
 right(90)
 forward(200)
 right(90)
+end_fill()
 
 penup()
 goto(-80,250)
@@ -164,8 +178,8 @@ pendown()
 
 
 forward(10)
-
-
+color("red")
+begin_fill()
 right(125)
 forward(230)
 
@@ -174,13 +188,15 @@ forward(230)
 
 right(120)
 forward(15)
-
+end_fill()
 
 
 penup()
 goto(550,25)
 pendown()
 
+color("white")
+begin_fill()
 right(95)
 forward(80)
 
@@ -193,11 +209,14 @@ forward(80)
 
 left(90)
 forward(90)
+end_fill()
 
 penup()
 goto(-440,30)
 pendown()
 
+color("white")
+begin_fill()
 left(90)
 forward(80)
 
@@ -212,11 +231,14 @@ forward(90)
 
 right(90)
 right(90)
+end_fill()
 
 penup()
 goto(-10,100)
 pendown()
 
+color("white")
+begin_fill()
 left(90)
 forward(100)
 
@@ -228,6 +250,8 @@ forward(100)
 
 right(90)
 forward(100)
+end_fill()
+
 
 right(90)
 forward(50)
@@ -235,6 +259,7 @@ forward(50)
 right(90)
 forward(100)
 
+
 right(90)
 forward(50)
 
@@ -243,6 +268,37 @@ forward(50)
 
 right(90)
 forward(100)
+
+
+right(90)
+right(90)
+left(90)
+
+
+# drawing a grass
+
+penup()
+goto(-835,-350)
+pendown()
+
+
+color("green")
+begin_fill()
+
+
+forward(1660)
+right(90)
+
+forward(130)
+right(90)
+
+forward(1660)
+right(90)
+forward(130)
+
+
+end_fill()
+# end of grass
 
 penup()
 goto(325,-420)
@@ -290,9 +346,204 @@ forward(60)
 left(90)
 forward(25)
 end_fill()
+#end of legs
+
+penup()
+goto(700,360)
+pendown()
+
+color("yellow")
+begin_fill()
+
+circle(60)
+
+end_fill()
+#end of the sun
+
+penup()
+goto(330,-315)
+pendown()
+
+color("blue")
+begin_fill()
+left(90)
+forward(100)
+
+left(90)
+forward(75)
+
+left(90)
+forward(100)
+
+left(90)
+forward(75)
+
+end_fill()
+
+color("blue")
+begin_fill()
+left(90)
+forward(75)
+
+right(120)
+forward(50)
+
+right(90)
+forward(15)
+
+right(90)
+forward(50)
+
+end_fill()
 
 
 
+right(90)
+right(90)
+forward(60)
+color("tan")
+begin_fill()
+left(90)
+forward(15)
 
+left(90)
+forward(10)
+
+left(90)
+forward(15)
+
+left(90)
+forward(10)
+left(30)
+
+
+end_fill()
+#end of hand
+
+penup()
+goto(295,-215)
+pendown()
+
+color("tan")
+begin_fill()
+
+
+circle(30)
+
+end_fill()
+
+#end of head
+
+color("black")
+
+penup()
+goto(280,-180)
+pendown()
+
+circle(2)
+
+penup()
+goto(310,-180)
+pendown()
+
+circle(2)
+
+
+color("brown")
+
+penup()
+goto(290,-190)
+pendown()
+
+
+left(45)
+forward(10)
+
+penup()
+goto(280,-200)
+pendown()
+
+left(90)
+right(90)
+right(90)
+circle(20,90)
+
+left(45)
+
+penup()
+goto(265,-160)
+pendown()
+color("yellow")
+begin_fill()
+forward(30)
+right(140)
+forward(20)
+
+left(100)
+forward(20)
+
+right(100)
+forward(20)
+
+left(100)
+forward(20)
+
+right(45)
+right(45)
+right(45)
+forward(30)
+
+right(95)
+forward(50)
+end_fill()
+
+
+penup()
+goto(330,-315)
+pendown()
+
+color("blue")
+
+forward(75)
+right(90)
+
+forward(75)
+
+color("blue")
+begin_fill()
+right(90)
+
+
+left(120)
+forward(50)
+
+left(90)
+forward(15)
+
+left(90)
+forward(50)
+
+end_fill()
+
+
+left(90)
+left(90)
+forward(60)
+color("tan")
+begin_fill()
+right(90)
+forward(15)
+
+right(90)
+forward(10)
+
+right(90)
+forward(15)
+
+right(90)
+forward(10)
+right(30)
+
+end_fill()
       
 exitonclick()
---
+++ Level 06/classwork/classwork00.py
@@ -0,0 +1,33 @@
+
+# task 1:Update this code, add the best practices we've all gone through so far, and send me the results on Discord
+
+
+
+
+dog_name1 = "Rick"
+
+dog_name2 = "John"
+dog_name2 = "Max"
+
+print("First dog's name is" + " " + dog_name1 + ". " + "Second dog's name is" + " " + dog_name2 + ".")
+
+print(f"First dog's name is {dog_name1}. Second dog's name is {dog_name2}.")
+
+# In this code I used three best practices: Snake_Case,f-str,comments
+
+# in this code, we first declared the variables and then concatenated those names to form a sentence
+
+
+
+
+
+# task 2: Create 2 variables, num1 and num2, input both numbers as user and output their sum
+
+
+num1 = int(input("enter the first number: "))
+num2 =int(input("enter the second number: "))
+
+
+print(num1 + num2)
+
+
