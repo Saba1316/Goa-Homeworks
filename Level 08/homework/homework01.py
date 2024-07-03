0a2f8c61e9a95b1cc9dcb2b7e90fa32b5d0a0e9a
TajMahal568 <giorgadzesaba48@gmail.com>
2024-07-03 14:01:45 +0400

    commit

--- Level 08/homework/homework01.py
+++ Level 08/homework/homework01.py
@@ -1,3 +1,44 @@
-# Task 2: Test all of this exactly as in the class, but using variables: ==, !=, >=, <=, >, <, and, or. and write in the comments what it will bring out (min-20 examples)
+# Task 2: Test all of this exactly as in the class, but using variables: ==, !=, >=, <=, >, <, and, or. and write in the comments what it will bring out (min-20 examples).
 
-# Didn't understand this homework
+
+# 1
+print(10 != 5) # True
+# 2
+print(13 != 13) # False
+# 3
+print(5 >= 6) # False
+# 4
+print(20 >= 20) # True
+# 5
+print(16 <= 13) # False
+# 6
+print(100 <= 100) # True
+# 7
+print(True and True) # True
+# 8
+print(False and True) # False
+# 9
+print(True or False) # True
+# 10
+print(False or False) # False
+# 11
+print(200 == 200) # True
+# 12
+print(120 == 200) # False
+# 13 
+print(5 < 3) # True
+# 14
+print(100 > 1000) # False
+# 15
+print(100 <= 100 or 16 <= 13 ) # True
+# 16
+print( 20 >= 20 or 5 >= 6) # True
+# 17
+print(200 == 200 and 120 == 200 ) # False
+# 18
+print( 10 != 5 or 13 != 13) 
+# 19
+print(True and True or 5 < 3 and 100 > 1000 ) # True
+# 20
+print(5 >= 6 or 120 == 200 and 100 > 1000  or 20 >= 20)
+
