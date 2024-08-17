9903e2d1e757be0032d6444be8139d26c6a9c923
TajMahal568 <giorgadzesaba48@gmail.com>
2024-08-17 11:36:27 +0400

    commit

---
+++ Level 26/homework/homework.py
@@ -0,0 +1,4 @@
+# Task 2: In the form of comments, describe what a tuple is, what it is used for, what properties it has.
+
+# Tuples are an essential data structure in Python, representing an immutable, ordered collection of elements. 
+# They are similar to lists but with a key difference: once a tuple is created, its contents cannot be changed. 
