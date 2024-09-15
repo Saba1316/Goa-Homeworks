b07477f23eda17f80cf80b3b0c7b23720a50cdfc
TajMahal568 <giorgadzesaba48@gmail.com>
2024-09-15 14:50:47 +0400

    commit

--- Level 34/homework/homework2.py
+++ Level 34/homework/homework2.py
@@ -2,11 +2,9 @@
 # (Handle exceptions without try/except. hint: use list comprehension for simplicity
 
 
-def sum_numbers(values):
+def sum_numbers(items):
+    
+    return sum(int(item) for item in items if isinstance(item, int) or (isinstance(item, str) and item.isdigit()))
 
-    return sum_numbers(int(values))
 
-
-values = [10,"10"]
-result = sum_numbers(values)
-print(result)
+print(sum_numbers([10, "10", "20", 30]))  
