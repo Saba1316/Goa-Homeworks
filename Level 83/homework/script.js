7b654e089bd59f6e2b18ed357eece646e1d4ebe6
TajMahal568 <giorgadzesaba48@gmail.com>
2025-03-04 17:57:20 +0400

    commit

---
+++ Level 83/homework/script.js
@@ -0,0 +1,19 @@
+// Find a little information about javascript modules
+
+// JavaScript modules are a way of organizing and structuring your 
+// code into separate files, which can then be imported and exported as 
+// needed. This helps keep your codebase more manageable, reusable, and easier to maintain.
+
+
+// 1. Why Use JavaScript Modules in Your Code?
+// a. Code Reusability:
+// One of the key reasons to use modules is to enable code reusability. Instead of rewriting the same functions 
+// across multiple files, you can place common logic in a module, then import it wherever it’s needed. This reduces redundancy and increases maintainability.
+
+// JavaScript modules are essential for building modern, maintainable, and scalable applications. By organizing your code into s
+// eparate files and importing/exporting functionality, you can improve reusability, maintainability, and performance. However, 
+// to maximize the benefits of modules, it’s important to follow best practices such as keeping modules small and focused, avoiding circular 
+// dependencies, and using tools to optimize your code for production.
+
+// Whether you’re building small web applications or large-scale enterprise systems, understanding how to write, 
+// structure, and manage JavaScript modules will be critical to your success as a developer.
